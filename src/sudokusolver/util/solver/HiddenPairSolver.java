57dd160b09e62ad60335ea3fcffc671487d2cf0b
Will McBurney <pmcburn1@gmail.com>
2022-01-26 22:54:37 -0500

    added template

---
+++ src/sudokusolver/util/solver/HiddenPairSolver.java
@@ -0,0 +1,13 @@
+package sudokusolver.util.solver;
+
+import sudokusolver.util.grid.SudokuGrid;
+
+public class HiddenPairSolver implements OneStepSolver {
+
+	@Override
+	public String solveOneStep(SudokuGrid g) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
